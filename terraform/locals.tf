923968ebb583a471df95df02a0d7d66bf8280aaf
Sasikala266 <tippanagarimanasa@gmail.com>
2026-03-20 15:47:35 +0000

    removing the additional buckets

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -7,8 +7,6 @@ locals {
   # Bucket names - must be globally unique in AWS
   bucket_names = {
     data    = "${local.name_prefix}-data-${data.aws_caller_identity.current.account_id}"
-    logs    = "${local.name_prefix}-logs-${data.aws_caller_identity.current.account_id}"
-    backups = "${local.name_prefix}-backups-${data.aws_caller_identity.current.account_id}"
   }
   
   # Common tags to be applied to all resources
@@ -22,7 +20,5 @@ locals {
   # Bucket-specific configurations
   bucket_configs = {
     data    = { versioning = true, encryption = true }
-    logs    = { versioning = false, encryption = true }
-    backups = { versioning = true, encryption = true }
   }
 }
